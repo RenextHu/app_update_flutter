44d545a7f40fb8e12016b14c2d42084524ec1ffd
hubei_0721 <hubei_0721@126.com>
2022-02-09 10:21:16 +0800

    //

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter_app_upgrade/flutter_app_upgrade.dart';
-
+//
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
